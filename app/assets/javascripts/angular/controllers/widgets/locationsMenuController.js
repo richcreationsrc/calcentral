011a5bb76478f2003100d9ad869274b5ebdd746c
Richard Chen <rlc11752@gmail.com>
2014-05-22 14:29:59 -0700

    test

--- app/assets/javascripts/angular/controllers/widgets/locationsMenuController.js
+++ app/assets/javascripts/angular/controllers/widgets/locationsMenuController.js
@@ -4,27 +4,14 @@
   /**
    * Campus controller
    */
-  angular.module('calcentral.controllers').controller('locationsMenuController', function($routeParams, $scope, $http) {
+  angular.module('calcentral.controllers').controller('locationsMenuController', function($scope, $http) {
 
-    $scope.loadLocations = function() {
+    var loadLocations = function() {
       $http.get('/dummy/json/locations.json').success(function(data) {
         angular.extend($scope, data);
       });
-
-      $scope.value = 'closest'; //set a default value for dropdown when page first loads
-      $scope.showHide = true; //this will allow the initial message to show; once set to false, it will disappear
     };
 
-    $scope.updateSelectedLocations = function(value, locations) {
-      $scope.showHide = false;
-      $scope.selectedLocations = [];
-
-      for (var i = 0; i < value; i++) {
-        $scope.selectedLocations.push(locations[i]);
-      }
-
-      return $scope.selectedLocations;
-    };
-$scope.loadLocations();
+    loadLocations();
   });
 })(window.angular);
